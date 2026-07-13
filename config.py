17cbb90fd0676ec54ad38ee945ffe3074b3f7d4c
kemalnuriyeff-debug <kemalnuriyeff@gmail.com>
2026-07-13 13:40:23 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+APP_NAME = "UNEC Smart Library AI"
+
+UNEC_ALISA = "https://e-library.unec.edu.az/alisaweb/#section-topline-2"
+
+MAX_RECOMMENDATIONS = 5
+
+HISTORY_LIMIT = 100
+
+USER_MEMORY = "data/users.json"
+
+USER_HISTORY = "data/history.json"
